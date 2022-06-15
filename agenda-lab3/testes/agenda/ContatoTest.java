3775538b67254e818fa6226941acc5a72125202a
Miguel <miguel.rodrigues@ccc.ufcg.edu.br>
2022-06-15 09:51:36 -0300

    salvando

---
+++ agenda-lab3/testes/agenda/ContatoTest.java
@@ -0,0 +1,14 @@
+package agenda;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContatoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
