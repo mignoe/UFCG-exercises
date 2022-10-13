6e107530892068fbf3d696bf65aefe300541d2da
Miguel Rodrigues <miguel.rodrigues@ccc.ufcg.edu.br>
2022-10-13 07:52:37 -0300

    organizing

--- EDA/exerciciosExtras/testes.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class testes {
-    public static void main(String[] args) {
-        int[] x = new int[0];
-        System.out.println(Arrays.toString(x));
-    }
-}
