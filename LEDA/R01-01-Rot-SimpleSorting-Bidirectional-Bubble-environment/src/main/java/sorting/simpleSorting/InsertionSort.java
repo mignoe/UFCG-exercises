5fac922492619e9c044aa66188fd29fd4c6e1b76
Miguel Rodrigues <miguel.rodrigues@ccc.ufcg.edu.br>
2022-10-18 09:34:33 -0300

    adding R01-01 - simpleSorting

---
+++ LEDA/R01-01-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -0,0 +1,31 @@
+package sorting.simpleSorting;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * As the insertion sort algorithm iterates over the array, it makes the
+ * assumption that the visited positions are already sorted in ascending order,
+ * which means it only needs to find the right position for the current element
+ * and insert it there.
+ */
+public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		// TODO Auto-generated method stub
+		if(leftIndex < 0 || rightIndex >= array.length) {
+			
+		}
+		
+		if (leftIndex < rightIndex) {
+			for (int i = leftIndex + 1; i <= rightIndex; i++) {
+				int j = i;
+				while (j != 0 && array[j].compareTo(array[j - 1]) < 0) {
+						Util.swap(array, j, j - 1);
+						j--;
+				}
+			}
+		}
+	}
+}
